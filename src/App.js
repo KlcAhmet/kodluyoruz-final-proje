01fed23ca788e50d380386727f3decd2accdb344
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-11 22:20:40 +0300

    update importlar eklendi

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+/* componenet */
+
+/* css */
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
