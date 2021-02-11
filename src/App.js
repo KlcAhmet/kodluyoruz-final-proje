b3103fd4da2a69f78831eaaf8a4683f380e77ee4
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-11 22:53:59 +0300

    update router eklendi

--- src/App.js
+++ src/App.js
@@ -7,16 +7,42 @@ import {
 } from "react-router-dom"
 
 /* componenet */
-
+import Header from "./components/Header"
 /* css */
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+
+  const [headerLinks, setHeaderLinks] = useState([])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3004/headerLinks')
+      .then(function ({ data }) {
+        setHeaderLinks(data)
+      })
+      .catch(function (error) {
+        console.log("Hata")
+        console.log(error)
+      })
+  }, [])
+
   return (
-    <div>
-      test
-    </div>
+    <Router>
+      <Header links={headerLinks} />
+
+      <main>
+        <Switch>
+          <Route path="/contagion" />
+          <Route path="/Symptoms" />
+          <Route path="/prevention" />
+          <Route path="/">
+          </Route>
+        </Switch>
+      </main>
+
+    </Router >
   );
 }
 
