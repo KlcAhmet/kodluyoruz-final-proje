653621c92a20148859aaa4fed835e9270d467776
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-14 02:13:08 +0300

    update componentmap klasörüne taşındı

---
+++ src/componentmap/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from "../components/Header"
+export { default as Overview } from "../components/Overview"
+export { default as Contagion } from "../components/Contagion"
