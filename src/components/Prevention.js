2603b241181d4cda0bcd9052c1a928b2bc6b99d7
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-14 04:41:42 +0300

    update import'lar eklendi

--- src/components/Prevention.js
+++ src/components/Prevention.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { Title, Subtitle, Description, OverviewImage, CardTitle } from "../componentmap/maincomponents"
+
 function Prevention() {
     return (
         <div>test</div>
