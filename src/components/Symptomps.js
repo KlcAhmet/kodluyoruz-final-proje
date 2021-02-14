f39524569eed7e94464fa33b614dadad43cf4775
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-14 04:11:10 +0300

    update Symptomps içerikleri eklendi

--- src/components/Symptomps.js
+++ src/components/Symptomps.js
@@ -1,4 +1,33 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { Title, Subtitle, Description, OverviewImage } from "../componentmap/maincomponents"
+
 function Symptoms() {
+    const [info, setInfo] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3004/symptompsPage')
+            .then(function ({ data }) {
+                setInfo(data)
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }, [])
+
+    return (
+        <section>
+            <div className="info">
+                <Title cls="info__title" title={info.title} />
+                <Subtitle cls="info__subtitle" title={info.subtitle} />
+                <Description cls="info__description" title={info.description} />
+            </div>
+            <figure>
+                <OverviewImage src={info.imagesrc} cls="card-img-top" />
+            </figure>
+        </section>
+    )
 
 }
 
