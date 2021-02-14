8128ccb872fefdacd947161db1a214997c5534eb
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-14 04:10:32 +0300

    update Symptomps componenti eklendi

--- src/componentmap/index.js
+++ src/componentmap/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "../components/Header"
 export { default as Overview } from "../components/Overview"
 export { default as Contagion } from "../components/Contagion"
+export { default as Symptomps } from "../components/Symptomps"
