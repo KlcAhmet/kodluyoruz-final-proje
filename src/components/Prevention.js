6f53d10213bbc2a2d70cc10dbd3ef7b0c35a7a51
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-15 23:17:51 +0300

    update icerik eklendi

--- src/components/Prevention.js
+++ src/components/Prevention.js
@@ -3,8 +3,61 @@ import axios from "axios"
 import { Title, Subtitle, Description, OverviewImage, CardTitle } from "../componentmap/maincomponents"
 
 function Prevention() {
+    const [header, setHeader] = useState([])
+    const [information, setInformation] = useState([])
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3004/preventionPage')
+            .then(function ({ data }) {
+                const result = []
+                setHeader(data)
+                data.information.forEach(({ id, title, description, imagesrc }) => {
+                    console.log(imagesrc)
+                    result.push(resultHTML(id, title, description, imagesrc))
+                });
+                setInformation(result)
+                console.log(data)
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }, [])
+    /* console.log(information) */ // -------------
+
     return (
-        <div>test</div>
+        <section>
+            <div className="info">
+                <Title cls="info__title" title={header.title} />
+                <Subtitle cls="info__subtitle" title={header.subtitle} />
+                <Description cls="info__description" title={header.description} />
+            </div>
+            <div>
+                {information}
+            </div>
+        </section>
+    )
+}
+
+
+
+function resultHTML(id, title, description, imagesrc) {
+    return (
+        <section key={id}>
+            <div className="head row">
+                <div className="info col-12 col-md-6">
+                    <Subtitle cls="info__subtitle" title={id} />
+                    <CardTitle cls="info__title" title={title} />
+                    <Description cls="info__desc" title={description} />
+                </div>
+                <div className="head__img col-12 col-md-6">
+                    <figure>
+                        <OverviewImage cls="" src={imagesrc} />
+                    </figure>
+                </div>
+            </div>
+        </section>
     )
 }
 
