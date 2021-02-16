4bf16e56ea434a01f83c26d82c002c794023434c
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-16 22:02:02 +0300

    update içerik yazıldı

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,81 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { Description, OverviewImage, CardTitle } from "../componentmap/maincomponents"
+
 function Statistics() {
+    const [map, setMap] = useState()
+    const [arrows, setArrows] = useState([])
+    const [flag, setFlag] = useState([])
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3004/istatistic')
+            .then(function ({ data }) {
+                const result = []
+                setMap(data.map)
+                setArrows(data.arrows)
+                data.flag.forEach(({ id, name, imagesrc }) => {
+                    result.push(flags(id, name, imagesrc, arrows.upArrow))
+                });
+                setFlag(result)
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }, [])
+
     return (
-        <div>
-            test
+        <section>
+            <div className="row">
+                <div className="col-12 col-md-8">
+                    <figure>
+                        <OverviewImage src={map} />
+                    </figure>
+                </div>
+                <div className="col-12 col-md-4">
+                    <section>
+                        <div className="row col-12">
+                            <div className="col-6">
+                                <CardTitle cls="" title="Live Reports" />
+                            </div>
+                            <div className="col-6">
+                                <figure style={{ width: 60 }}>
+                                    <OverviewImage src={arrows.mapStatArrow} />
+                                </figure>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        {flag}
+                    </section>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+function flags(id, name, imagesrc, arrow) {
+
+
+    return (
+        <div key={id} className="stats row col-12">
+            <div className="flag col-2">
+                <OverviewImage cls="" src={imagesrc} />
+            </div>
+            <div className="country col-4">
+                <CardTitle cls="" title={name} />
+            </div>
+            <div className="rate col-4">
+                <Description cls="" title="0000" />
+            </div>
+            <div className="arrow col-2">
+                <OverviewImage src={arrow} cls="" />
+            </div>
         </div>
     )
 }
+
 
 export default Statistics
