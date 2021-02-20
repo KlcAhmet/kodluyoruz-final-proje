0ad5d17f3daa27a9ca163205d6c2eab161ade02e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-20 22:36:06 +0300

    update return test içerik eklendi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Logo from "./Logo"
 import HeaderLinks from "./HeaderLinks"
+import { Description } from "../componentmap/maincomponents"
 
 function Footer() {
 
@@ -27,14 +28,19 @@ function Footer() {
         <footer>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-3">
+                    <div className="col-12 col-sm-3 align-self-center">
                         <Logo />
                     </div>
-                    <div className="col-12 col-sm-6">
+                    <div className="col-12 col-sm-6 align-self-center">
                         <HeaderLinks />
                     </div>
-                    <div className="col-12 col-sm-3">
+                    <div className="col-12 col-sm-3 align-self-center">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, et ducimus. Fuga, illo ut temporibus eveniet perferendis laboriosam tenetur eius iste a aperiam? Aliquid neque odit quod illo qui? Accusantium?
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-center">
+                        <small>Bu web sitesinin <a href="https://github.com/KlcAhmet/kodluyoruz-final-proje" target="_blank" rel="noopener noreferrer">kaynak kodlarına</a> Github üzerinden ulaşabilirsiniz ❤️</small>
                     </div>
                 </div>
             </div>
