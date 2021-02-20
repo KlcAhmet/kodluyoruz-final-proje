cdad2615f0ba32f5b1fe72292bba51fba57dc7bb
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-20 23:19:04 +0300

    update içerik yazıldı

--- src/components/FooterLogo.js
+++ src/components/FooterLogo.js
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import { OverviewImage } from "../componentmap/maincomponents"
 
 function FooterLogo() {
+    const [footerLink, setFooterLink] = useState([])
 
+    useEffect(() => {
+        axios.get('http://localhost:3004/footerLinks')
+            .then(function ({ data }) {
+                console.log(data)
+                const result = []
+                data.forEach(({ id, imagesrc, link }) => {
+                    const temp = <a className="" key={id} href={link} target="_blank"><OverviewImage cls="" src={imagesrc} /></a>
+                    result.push(temp)
+                });
+                setFooterLink(result)
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }, [])
+
+    return (
+        <>
+            {footerLink}
+        </>
+    )
 }
 
 export default FooterLogo
