b6c0f57f17c6bf6432ce1c48f334455936d4cbf2
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-21 02:50:23 +0300

    update bootstap sınıfları güncellendi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import { Logo, HeaderLinks, ContactButton } from "../componentmap/maincomponents"
+import '../css/Header.css'
 
 function Header() {
     return (
         <header>
             <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-12 col-sm-12 col-md-3">
+                <div className="header row align-items-center">
+                    <div className="col-12 col-sm-12 col-lg-3">
                         <Logo />
                     </div>
-                    <div className="col-12 col-sm-12 col-md-9">
+                    <div className="col-12 col-sm-12 col-lg-8">
                         <HeaderLinks />
+                    </div>
+                    <div className="col-12 col-sm-12 col-lg-1">
                         <ContactButton />
                     </div>
                 </div>
