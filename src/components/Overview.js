4822d57ac7b759c9c3132c9260a1a2c4a4f4013e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-21 04:34:34 +0300

    update className değişti

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { Title, Subtitle, Description, OverviewImage, CommonButton } from "../componentmap/maincomponents"
+import '../css/Overview.css'
 
 function Overview() {
 
@@ -30,14 +31,14 @@ function Overview() {
 function resultHTML(i, title, subtitle, description, imagesrc, buttontitle) {
     return (
         <section key={i}>
-            <div className="head row">
+            <div className="content row align-items-center">
                 <div className="info col-12 col-md-6">
                     <Title cls="info__title" title={title} />
                     <Subtitle cls="info__subtitle" title={subtitle} />
                     <Description cls="info__desc" title={description} />
                     <CommonButton link="#" cls="common-button" text={buttontitle} />
                 </div>
-                <div className="head__img col-12 col-md-6">
+                <div className="content__img col-12 col-md-6">
                     <figure>
                         <OverviewImage src={imagesrc} />
                     </figure>
