5071082f32151adec06e82d32cc895fecf0911bb
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-27 02:20:53 +0300

    update final initalize

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -1,19 +1,14 @@
 import '../css/Darkmode.css'
+import { selection } from '../js/darkmode.js'
 
 function Darkmode() {
-
     return (
         <div className="darkmode col-12">
-            <input onClick={teest} className="darkmode__checkbox" type="checkbox" id="s3" />
+            <input onClick={selection} className="darkmode__checkbox" type="checkbox" id="s3" />
             <label className="slider-v2" htmlFor="s3"></label>
         </div>
     )
 }
 
-
-function teest() {
-    const aa = document.getElementById("s3")
-    console.dir(aa)
-}
 
 export default Darkmode
