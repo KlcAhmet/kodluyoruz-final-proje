e0a48df80410c94cd6ac4f0c7d4797f023460544
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-26 21:51:15 +0300

    update Dakmode componenti eklendi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
-import { Logo, HeaderLinks, ContactButton } from "../componentmap/maincomponents"
+import { Logo, HeaderLinks, ContactButton, Darkmode } from "../componentmap/maincomponents"
 import '../css/Header.css'
 
 function Header() {
     return (
         <header>
             <div className="container">
-                <div className="header row align-items-center">
+                <div className="header row col-12 align-items-center">
                     <div className="col-12 col-sm-12 col-lg-3">
                         <Logo />
                     </div>
-                    <div className="col-12 col-sm-12 col-lg-8">
+                    <div className="col-12 col-sm-12 col-lg-7">
                         <HeaderLinks />
                     </div>
-                    <div className="col-12 col-sm-12 col-lg-1">
+                    <div className="col-12 col-sm-12 col-lg-2">
                         <ContactButton />
                     </div>
+                    <Darkmode />
                 </div>
             </div>
         </header>
