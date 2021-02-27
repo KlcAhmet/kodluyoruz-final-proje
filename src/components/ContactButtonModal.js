b72039c2d7a378325380c51876b4ceddcf920d3b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-27 23:42:04 +0300

    import içerik yazıldı

--- src/components/ContactButtonModal.js
+++ src/components/ContactButtonModal.js
@@ -1,7 +1,27 @@
+import { CommonButton } from "../componentmap/maincomponents"
+
 function ContactButtonModal() {
     return (
-    
-)
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h5 className="modal-body__title">Have Question in mind?</h5>
+                        <span className="modal-body__subtitle">Let us help you</span>
+                        <div className="modal-body__mail-block">
+                            <input type="email" placeholder="snmonydemo@gmail.com" />
+                            <CommonButton link="#" cls="common-button common-button--dayBackGround" text="Send" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ContactButtonModal
