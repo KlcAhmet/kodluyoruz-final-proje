39e0d5e859ae738caffe3e41ddf153757d281165
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-27 23:41:18 +0300

    update button haline getirildi

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -1,14 +1,13 @@
+import ContactButtonModal from "./ContactButtonModal"
+
 function ContactButton() {
     return (
         <div className="contact">
             <button type="button" className="contact__btn" data-toggle="modal" data-target="#exampleModalCenter">Contact</button>
+            <ContactButtonModal />
         </div>
+
     )
 }
 
 export default ContactButton
-
-
-/* <div className="contact">
-           <a href="mailto:mailadress@example.com">Contact</a>
-       </div> */
