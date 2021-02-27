a3a39f41873e4556a67cb20cfbcabdb32a7931e5
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-27 21:25:28 +0300

    update className eklendi

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -1,9 +1,14 @@
 function ContactButton() {
     return (
         <div className="contact">
-            <a href="mailto:mailadress@example.com">Contact</a>
+            <button type="button" className="contact__btn" data-toggle="modal" data-target="#exampleModalCenter">Contact</button>
         </div>
     )
 }
 
 export default ContactButton
+
+
+/* <div className="contact">
+           <a href="mailto:mailadress@example.com">Contact</a>
+       </div> */
