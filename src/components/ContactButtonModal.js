429cddf90c4ff0a08b5e7fe82f934b13956972b2
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-01 23:38:03 +0300

    fix class hataları düzeltildi

--- src/components/ContactButtonModal.js
+++ src/components/ContactButtonModal.js
@@ -3,15 +3,15 @@ import '../css/ContactButtonModal.css'
 
 function ContactButtonModal() {
     return (
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
+                    <div className="modal-body">
                         <h5 className="modal-body__title">Have Question in mind?</h5>
                         <span className="modal-body__subtitle">Let us help you</span>
                         <div className="modal-body__mail-block row">
